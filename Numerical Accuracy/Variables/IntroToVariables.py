fa0ae2c395e62b2c7b8d2b2c6387d271a156834f
Javaria Ghafoor <48168616+Javaria-Ghafoor@users.noreply.github.com>
2019-11-16 00:33:31 -0800

    Update and rename Numerical Accuracy/Variables.py to Numerical Accuracy/Variables/IntroToVariables.py

--- Numerical Accuracy/Variables/IntroToVariables.py
+++ Numerical Accuracy/Variables/IntroToVariables.py
@@ -8,13 +8,13 @@ b = 2.0      # float
 c = 2 + 7j   # complex
 d = 'hello'  # str
 #   use of type() function
-print (a, ' belongs to ', type(a))
-print (b, ' belongs to ', type(b))
-print (c, ' belongs to ', type(c))
-print (d, ' belongs to ', type(d))
+print(a, ' belongs to ', type(a))
+print(b, ' belongs to ', type(b))
+print(c, ' belongs to ', type(c))
+print(d, ' belongs to ', type(d))
 #   use of isinstance() function
-print (c, 'is a complex number?: ', isinstance(c, complex))
-print (d, 'is a complex number?: ', isinstance(d, complex))
+print(c, 'is a complex number?: ', isinstance(c, complex))
+print(d, 'is a complex number?: ', isinstance(d, complex))
 """
     Now, since int, float, complex etc are inbuilt classes, therefore they have an
     inbuilt constructor.
@@ -26,10 +26,14 @@ print (d, 'is a complex number?: ', isinstance(d, complex))
 x = int()       # an object of class int() gets created on execution of this line
 y = float()
 z = complex()
-#   inputting variables.. Python 3.7 uses input() while Pyhton2.7 uses raw_input()
-x = input('Enter an integer: ')
-y = input('Enter a floating point number: ')
-z = input('Enter a complex number: ')
-print ('x = ', x)
-print ('y = ', y)
-print ('z = ', z)
+#   inputting variables.. Python 3.7 uses input() while Python 2.7 uses raw_input()
+"""
+    Note that one can enter any data type here as an input to 'p', the statement 'Enter an integer' does not bound the
+    user from entering some string or floating number etc. The same goes for 'q' and 'r'
+"""
+p = input('Enter an integer: ')
+q = input('Enter a floating point number: ')
+r = input('Enter a complex number: ')
+print('p = ', p)
+print('q = ', q)
+print('r = ', r)

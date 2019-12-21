068ff9d89ac3ca39ac6755be866c01ed1fcb1767
Javaria Ghafoor <48168616+Javaria-Ghafoor@users.noreply.github.com>
2019-12-21 12:06:32 +0500

    Create SimpsonsRule.py

---
+++ Numerical Integration/2. Simpsons Rule/SimpsonsRule.py
@@ -0,0 +1,23 @@
+from math import *
+
+# example function
+def func(x):
+    return (x**3)*exp(-x)
+
+
+def Simpson(f, a, b, n):  # Simpson Rule
+    if n % 2 == 0:
+        n += 1     # to ensure that n is odd
+    h = (b-a)/(n-1)      # no. of intervals = n - 1
+    s1 = s2 = 0
+    for i in range(1, n-3, 2):
+        s1 += f(a+i*h)
+    for i in range(2, n-2, 2):
+        s2 += f(a+i*h)
+    return (h/3)*(f(a)+4*s2+2*s1+f(b))
+
+
+a = 0       # lower limit of integral
+b = 1       # upper limit of integral
+n = 200
+print('i=', Simpson(func, a, b, n))

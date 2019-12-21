2b0562885cf205fcc046903320fcb201dad596fa
Javaria Ghafoor <48168616+Javaria-Ghafoor@users.noreply.github.com>
2019-12-21 14:47:59 +0500

    Update TrapezoidalRule.py

--- Numerical Integration/1. Trapezoidal Rule/TrapezoidalRule.py
+++ Numerical Integration/1. Trapezoidal Rule/TrapezoidalRule.py
@@ -10,15 +10,15 @@ def func2(x):       # argument x not being ued in body of this function
 
 
 def Trapz(f, a, b, n):       # trapezoidal rule
-    h = (b-a)/(n-1)
+    h = (b-a)/(n-1)     # no. of trapezoids = n - 1
     I = (1/2)*(f(a)+f(b))*h    # initialize I
-    for j in range(1, n):
-        I += f(a+j*h)*h      # I = I + func(a+j*h), keep updating I
+    for i in range(2, n):
+        I += f(a+(i - 1)*h)*h      # I = I + func(a+j*h), keep updating I, j = i - 1
     return I
 
 
 a = 0    # lower limit of integral
 b = 1    # upper limit of integral
-n = 200  # no. of trapezoids = n - 1
+n = 200  
 print('Integral of func1(x)=', Trapz(func1, a, b, n))
 print('Integral of func2(x)=', Trapz(func2, a, b, n))
